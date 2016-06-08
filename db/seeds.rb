57a07810dabec81806e9c6a16c4002bd57bc4c2c
chrisfcallaway <chrisfcallaway@gmail.com>
2016-06-07 21:34:16 -0500

    commit 14 - slight changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,22 +23,47 @@ proposal_info = [
 {
     :image => "https://upload.wikimedia.org/wikipedia/commons/1/17/DowneyMcdonalds.jpg",
     :store_name => "McDonalds"
+    :description => "Good ole fashioned burger joint"
+    :square_feet => "20000"
+    :nearest => "2"
+    :bid => "100000"
+    :architectural => "Yes"
   },
   {
     :image => "https://upload.wikimedia.org/wikipedia/commons/3/31/Walmart_Home_Office.jpg",
     :store_name => "Wal-Mart"
+    :description => "The big general store"
+    :square_feet => "200000"
+    :nearest => "5"
+    :bid => "10000000"
+    :architectural => "No"
   },
   {
     :image => "https://upload.wikimedia.org/wikipedia/en/9/97/Arby%27s-Midland-MI.jpg",
     :store_name => "Arby's"
+    :description => "Taste the deliciousness"
+    :square_feet => "25000"
+    :nearest => "4"
+    :bid => "70000"
+    :architectural => "Yes"
   },
   {
     :image => "https://upload.wikimedia.org/wikipedia/commons/d/d8/2009-04-12_The_Home_Depot_in_Knightdale.jpg",
     :store_name => "Home Depot"
+    :description => "Buy crap you don't need!"
+    :square_feet => "400000"
+    :nearest => "20"
+    :bid => "2000000"
+    :architectural => "Maybe"
   },
   {
     :image => "https://upload.wikimedia.org/wikipedia/commons/5/5f/Red_Lobster%2C_Baton_Rouge.jpg",
     :store_name => "Red Lobster"
+    :description => "Movie quote made me famous"
+    :square_feet => "40000"
+    :nearest => "12"
+    :bid => "300000"
+    :architectural => "Yes"
   }
 ]
 
